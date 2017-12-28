a2699fc5edadb6e48a3af01a30132277b918ceb7
Tiago Mendes <tiagoomendess@gmail.com>
2017-12-28 17:25:22 +0000

    asdsads

---
+++ Utentes/IUtentes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Utentes
+{
+    // OBSERVAÇÃO: Você pode usar o comando "Renomear" no menu "Refatorar" para alterar o nome da interface "IUtentes" no arquivo de código e configuração ao mesmo tempo.
+    [ServiceContract]
+    public interface IUtentesSOAP
+    {
+        [OperationContract]
+        bool IsWorkingSOAP();
+
+        [OperationContract]
+        void DoWorkSOAP();
+    }
+
+    [ServiceContract]
+    public interface IUtentesREST
+    {
+
+        [OperationContract]
+        bool IsWorkingREST();
+
+        [OperationContract]
+        void DoWorkREST();
+    }
+}
---
+++ Utentes/Utentes.svc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Utentes
+{
+    // OBSERVAÇÃO: Você pode usar o comando "Renomear" no menu "Refatorar" para alterar o nome da classe "Utentes" no arquivo de código, svc e configuração ao mesmo tempo.
+    // OBSERVAÇÃO: Para iniciar o cliente de teste do WCF para testar esse serviço, selecione Utentes.svc ou Utentes.svc.cs no Gerenciador de Soluções e inicie a depuração.
+    public class Utentes : IUtentesSOAP, IUtentesREST
+    {
+
+        public void DoWorkREST()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DoWorkSOAP()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsWorkingREST()
+        {
+            return true;
+        }
+
+        public bool IsWorkingSOAP()
+        {
+            return true;
+        }
+    }
+}
